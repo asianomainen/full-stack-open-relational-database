1bf22710da79a10402090020addfa80910d737c3
asianomainen <oskari.nuottonen@gmail.com>
2023-02-15 15:19:38 +0200

    Exercises 13.1-13.3

---
+++ commands.sql
@@ -0,0 +1,2 @@
+INSERT INTO blogs (author, url, title, likes) VALUES ('Martti Ahtisaari', 'www.martti.fi', 'Martin muistelot', 99);
+INSERT INTO blogs (url, title) VALUES ('www.mystery.fi', 'Tarjan muistelot');
